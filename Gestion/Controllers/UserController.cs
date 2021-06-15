c73d83c73bf9b90992a0c40720b6d8a9ab63ade4
FranciscoMac <francisco.83@gmail.com>
2021-06-14 22:07:33 -0300

    Sistema de Gestion, prueba de Visual studio Code con Entity Framework

---
+++ Gestion/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Gestion.DBContexts;
+using Gestion.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private MyDBContext myDbContext;
+
+        public UserController(MyDBContext context)
+        {
+            myDbContext = context;
+        }
+
+        [HttpGet]
+        public IList<User> Get()
+        {
+            return (this.myDbContext.Users.ToList());
+        }
+    }
+}
---
+++ Gestion/Controllers/UserGroupController.cs
@@ -0,0 +1,29 @@
+﻿using Gestion.DBContexts;
+using Gestion.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gesion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserGroupController : ControllerBase
+    {
+        private MyDBContext myDbContext;
+
+        public UserGroupController(MyDBContext context)
+        {
+            myDbContext = context;
+        }
+
+        [HttpGet]
+        public IList<UserGroup> Get()
+        {
+            return (this.myDbContext.UserGroups.ToList());
+        }
+    }
+}
